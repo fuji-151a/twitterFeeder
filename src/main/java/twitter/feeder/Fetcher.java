4172bdb29a66f267d5ff123fa4a03bf45598af27
yuya <yuya@fuji-PC>
2014-08-14 00:38:12 +0900

    file書き込みの機能を追加.

--- src/main/java/twitter/feeder/Fetcher.java
+++ src/main/java/twitter/feeder/Fetcher.java
@@ -1,6 +1,7 @@
 package twitter.feeder;
 
-import java.io.UnsupportedEncodingException;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 
 import kafka.consumer.KafkaStream;
@@ -14,25 +15,68 @@ import twitter.feeder.kafka.KafkaConsumer;
  */
 public class Fetcher {
 
+    /** output fileName. */
+    private String fileName;
+
+    /** 改行コード. */
+    private static String BR = System.getProperty("line.separator");
     /**
      * KafkaからTwitterのデータをFetchする.
      * @param args 1:confFile
-     * @throws UnsupportedEncodingException : 指定された文字セットがサポートされてない場合
+     * @throws Exception FileIO
      */
     public static void main(final String[] args)
-            throws UnsupportedEncodingException {
+            throws Exception {
         if (args.length < 1) {
             System.out.println("Please set ConfFile");
             System.exit(1);
         }
         KafkaConsumer kc = new KafkaConsumer(args[0]);
+        Fetcher f = new Fetcher();
+        f.setFileName(args[1]);
         List<KafkaStream<byte[], byte[]>> streams = kc.consume();
         for (KafkaStream<byte[], byte[]> stream : streams) {
             for (MessageAndMetadata<byte[], byte[]> msg : stream) {
                 String data = new String(msg.message(), "UTF-8");
-                System.out.println(data);
+                f.write(data);
             }
         }
     }
 
+    /**
+     * file書き込み.
+     * @param msg 書き込みデータ.
+     * @throws Exception ファイルの読み込み失敗.
+     */
+    private void write(final String msg) throws Exception {
+        File file = new File(fileName);
+        file.createNewFile();
+        if (checkBeforeWriteFile(file)) {
+            FileWriter fw = new FileWriter(file, true);
+            fw.write(msg + BR);
+            fw.close();
+        }
+    }
+
+    /**
+     * ファイルの存在とファイル書き込み，ファイル華道家の確かめ.
+     * @param file file.
+     * @return boolean
+     */
+    private boolean checkBeforeWriteFile(final File file) {
+        if (file.exists()) {
+            if (file.isFile() && file.canWrite()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * fileNameのSetter.
+     * @param fName fileの名前.
+     */
+    private void setFileName(final String fName) {
+        this.fileName = fName;
+    }
 }
